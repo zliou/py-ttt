12ad130955c21434fed9f5833d86c32a347194e2
Zach Liou <zachliou@gmail.com>
2023-07-22 00:31:49 -0700

    Add basic instructions rendering

--- printer.py
+++ printer.py
@@ -1,16 +1,20 @@
 from typing import List
 
 
-def clear_screen() -> None:
+def clear_screen():
     print("\033c", end="")
 
 
-def render_board() -> None:
+def render_board():
     print("   |   |   ")
     print("---+---+---")
     print("   |   |   ")
     print("---+---+---")
     print("   |   |   ")
+
+
+def render_instructions():
+    print("Enter [x] to quit")
 
 
 clear_screen()
